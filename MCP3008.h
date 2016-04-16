6f31c856e43b95d448c427a24a349952915c6518
David Andrs <andrsd@gmail.com>
2016-04-16 13:17:33 -0600

    Code formatting + adding begin() method

--- MCP3008.h
+++ MCP3008.h
@@ -2,23 +2,25 @@
   MCP3008.h - Library for communicating with MCP3008 Analog to digital converter.
   Created by Uros Petrevski, Nodesign.net 2013
   Released into the public domain.
-  
+
   ported from Python code originaly written by Adafruit learning system for rPI :
   http://learn.adafruit.com/send-raspberry-pi-data-to-cosm/python-script
 */
 
-#ifndef MCP3008_h
-#define MCP3008_h
+#ifndef MCP3008_H
+#define MCP3008_H
 
 #include "Arduino.h"
 
 class MCP3008
 {
-  public:
+public:
     MCP3008(int clockpin, int mosipin, int misopin, int cspin);
+    void begin(void);
     int readADC(int adcnum);
-  private:
-      int _clockpin, _mosipin, _misopin, _cspin;
+
+protected:
+    int _clockpin, _mosipin, _misopin, _cspin;
 };
 
 
